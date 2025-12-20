d6b74a1e474b7b748e5bd9b37a13e47eeb06d83a
primakt <dmaslennikov397@gmail.com>
2025-12-20 15:00:45 +0300

    users model

---
+++ app/models/users.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, BigInteger, String
+from ..base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    telegram_id = Column(BigInteger, unique=True, nullable=False, index=True)
+    username = Column(String, nullable=True)
+    first_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=True)
+    phone = Column(String, nullable=True)
+    role = Column(String, default="customer")
+    points = Column(Integer, default=0)
